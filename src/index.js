4f752d9d6a120a191bbef168c95d49cc6af2ac77
unknown <prathamjagga123@gmail.com>
2023-04-23 22:09:47 +0530

    remove un-necessary files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// this file creates the root of the react application and is the entry point of the app
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
