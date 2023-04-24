8e8cfc0484ca28d343d724396f4d4213b007420b
unknown <prathamjagga123@gmail.com>
2023-04-24 21:45:39 +0530

    remove header and changed global css name which was causing problems

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import React from "react";
 
-import "./Styles/Global.css";
+import "./Styles/App.css";
 
 import Search from "./Pages/Search";
-import Header from "./Components/Header";
 
 function App() {
   return (
-    <React.Fragment>
-      <Routes>
-        <Route path="/" Component={Search} />
-      </Routes>
-    </React.Fragment>
+    <Routes>
+      <Route path="/" Component={Search} />
+    </Routes>
   );
 }
 
